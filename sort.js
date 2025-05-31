9b85b5264ac61c53d711738b80e998380e648008
Kabir058 <riduanulkabir01@gmail.com>
2025-05-31 19:08:49 +0600

    sort sitd ascending and descending

--- sort.js
+++ sort.js
@@ -1,2 +1,6 @@
 let asort= ['rakib','sakib','nokib','akib']
 console.log(asort.sort())
+
+let num =[ 42,33,954,76,14,6,8,3,2,81,1,7]
+console.log(num.sort(function (a,b){return a-b}))
+console.log(num.sort(function(a,b){return b-a}))
