3d35dcbcc8577bda8c22d4f7db445441be9de589
Kabir058 <riduanulkabir01@gmail.com>
2025-05-31 20:34:59 +0600

    task 3

---
+++ task3.js
@@ -0,0 +1,18 @@
+
+// const numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// let result = '';
+
+// for (let name of numbers) {
+//     result += name;
+// }
+
+// console.log(result);
+
+const numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+let result = '';
+
+for (let i = 0; i < numbers.length; i++) {
+    result = result + numbers[i]; // 
+}
+
+console.log(result); 
